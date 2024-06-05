af7f78542fa9cd090dfa3b93e7ed30954dfd3f22
yunjeong <racheal0@snu.ac.kr>
2024-06-05 21:08:07 +0900

    modify config & script

--- PAT/run.sh
+++ PAT/run.sh
@@ -1,2 +1,16 @@
-# train PAT
+#!/bin/bash
+
+#SBATCH --job-name=both      
+#SBATCH --partition=a5000        
+#SBATCH --gres=gpu:1        
+#SBATCH --time=5-12:00:00
+#SBATCH --mem=100000
+#SBATCH --cpus-per-task=16
+#SBATCH --output=./out/market_both.log   
+
+ml purge
+ml load cuda/11.8
+eval "$(conda shell.bash hook)"  # Initialize Conda Environment
+conda activate pat             # Activate your conda environment
+
 python train.py --config_file "config/PAT.yml"
